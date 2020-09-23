11b1310f9e73dd0442dc3282e353b106c4df2877
Sigurd Odden <sigurdodden@gmail.com>
2020-09-23 14:00:33 +0200

    Another changes.

--- RSA_Func.py
+++ RSA_Func.py
@@ -1,14 +1,16 @@
 # Python file containing functions for RSA algorithm
 import random
 
-def gcd(x, y):
-    while y != 0:
-        (x, y) = (y, x % y)
-    return x
+def gcd(a,b):
+    if b == 0:
+        return a
+    else:
+        return gcd(b, a%b)
 
 def coprime(a, b):
     if gcd(a, b) == 1:
-        return b
-    # coprime(a,b+1)
+        return True
+    else:
+        return False
 
 
--- main.py
+++ main.py
@@ -1,14 +1,14 @@
-import math
-from random import *
 from sympy import *
 # Functions
 from RSA_Func import *
 
 #p = int(input("Enter prime number for p:"))
 #q = int(input("Enter prime number for q:"))
+#m = int(input("Enter message for m:"))
+
 p = 7 # 7
 q = 17 # 17
-
+m = 3
 
 #   Computing  n = pq
 n=p*q
@@ -19,36 +19,33 @@ phi = (p-1)*(q-1)
 print("phi =",phi)
 
 
-# Any number 1 < e < phi
-#e = randint(2,phi-1)
-#e = coprime(phi,1)
-
-# Generating random primal numbers between a and b
+# Any prime between 1 < e < phi
 minPrime = 0
-maxPrime = 1000
+maxPrime = 2000
 prime_list = [i for i in range(minPrime,maxPrime) if isprime(i)]
-e = random.choice([i for i in prime_list if p<i<q])
+e = random.choice([i for i in prime_list if 2<i<phi])
+
+e_1 = coprime(phi,e) #coprime check
+print("Coprime test: ",e_1)
 #e = 103
 print("E =",e)
 
 
 
-#d = mod_inverse(e,phi)
-d = pow(e,p,phi)
+d = mod_inverse(e,phi)
+#d = pow(e,p,phi)
 print("d =", d)
 
 
-#m = int(input("Enter message for m:"))
-m = 100
+
 # Keys
 print("Public key:",e,n)
 print("Private key:",d,n)
 
 print("-----\nMessage sent m:",m)
 
-# Encrypt & Decrypt
 
-#   Encrypt and decrypt
+#   Encryption and decryption
 Encrypt = (m**e)%n
 print("encrypt msg",Encrypt)
 
