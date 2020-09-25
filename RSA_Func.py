ea3484b18ee1d13a11f08bac802f9a778980af98
Sigurd Odden <sigurdodden@gmail.com>
2020-09-25 13:08:56 +0200

    Added fast modular exp. Added GCD iterator.

--- RSA_Func.py
+++ RSA_Func.py
@@ -1,16 +1,44 @@
 # Python file containing functions for RSA algorithm
-import random
 
+
+#GCD
 def gcd(a,b):
     if b == 0:
         return a
     else:
         return gcd(b, a%b)
+#GCD iteration method
+def gcd_iter(a,b):
+    while b!= 0:
+        (a,b) = (b,a % b)
+    return a
+
 
 def coprime(a, b):
     if gcd(a, b) == 1:
         return True
     else:
         return False
+
+#   https://crypto.stackexchange.com/questions/5889/calculating-rsa-private-exponent-when-given-public-exponent-and-the-modulus-fact
+def modinv(e, phi):
+    d_old = 0; r_old = phi
+    d_new = 1; r_new = e
+    while r_new > 0:
+        a = r_old // r_new
+        (d_old, d_new) = (d_new, d_old - a * d_new)
+        (r_old, r_new) = (r_new, r_old - a * r_new)
+    return d_old % phi if r_old == 1 else None
+
+
+def fast_exp(b, e, m):
+    r = 1
+    if 1 & e:
+        r = b
+    while e:
+        e >>= 1
+        b = (b * b) % m
+        if e & 1: r = (r * b) % m
+    return r
 
 
--- main.py
+++ main.py
@@ -2,15 +2,23 @@ from sympy import *
 # Functions
 from RSA_Func import *
 
-p = int(input("Enter prime number for p:"))
-q = int(input("Enter prime number for q:"))
-m = int(input("Enter message for m:"))
+#p = int(input("Enter prime number for p:"))
+#q = int(input("Enter prime number for q:"))
+#m = int(input("Enter message for m:"))
 print("--- Configuration ---\n")
-#p = 17
-#q = 41
-#m = 600
 
+
+p = 7
+q = 17
+m = 119
+pp = isprime(p)
+qq = isprime(q)
+if(pp and qq):
+    print("p and q is prime")
+else:
+    print("p and q is not prime")
 #   Computing  n = pq
+
 n=p*q
 print("n =", n)
 
@@ -18,21 +26,27 @@ print("n =", n)
 phi = (p-1)*(q-1)
 print("phi =",phi)
 
-
+#   Determining e
 #   Any prime between 1 < e < phi
 
-for e in range(2,phi):
-    if gcd(e,phi) == 1:
+for e in range(2,phi-1):
+    if gcd_iter(e,phi) == 1:
         break
 
+#   Testing if e and phi is co prime
+#   Return true if co prime, otherwise return false
 e_1 = coprime(phi,e)
-print("Coprime test: ",e_1)
+print("Co_Prime test: ",e_1)
 #e = 103
 print("E =",e)
 
 
+# d = e^-1 mod phi :  d = 1/e mod phi
 
-d = mod_inverse(e,phi)
+d = modinv(e,phi)
+
+
+#d = mod_inverse(e,phi)
 #d = pow(e,p,phi)
 print("d =", d)
 
@@ -41,17 +55,24 @@ print("--- Keys ---\n")
 
 # Keys
 print("Public key:",e,n)
-print("Private key:",d,n)
+print("Private key:",d,n,"\n")
 
-print("--- Encrypt/Decrypt ---\n")
+print("--- Encrypt/Decrypt ---")
 print("Message sent m:",m)
 
 
 #   Encryption and decryption
-Encrypt = (m**e)%n
+
+#
+#Encrypt = pow(m,e,n)
+Encrypt = fast_exp(m,e,n)
+#Encrypt = (m**e)%n
 print("encrypt msg",Encrypt)
 
 Decrypt = (Encrypt**d)%n
 print("decrypted msg ",Decrypt)
+
+
+
 
 
